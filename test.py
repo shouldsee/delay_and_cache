e8f8f697e1d49159dfb2ba558f44f346433152ab
teamchung <teamchung@example.com>
2019-08-22 15:35:51 +0000

    fix

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os,sys
 DIR= os.path.join(os.path.dirname(os.path.realpath(__file__)),'test_temp')
-os.makedirs(DIR)
+if not os.path.isdir(DIR):
+    os.makedirs(DIR)
 os.chdir(DIR)
-
 #os.chdir(os.path.dirname(os.path.realpath(__file__)))
-execfile("delay_and_cache.py")
+execfile(DIR + "/../delay_and_cache.py")

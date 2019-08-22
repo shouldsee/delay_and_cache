511c72ed138770e9ea15dd764acdd4e3175f1736
teamchung <teamchung@example.com>
2019-08-22 15:34:38 +0000

    test install

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 import os,sys
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
+DIR= os.path.join(os.path.dirname(os.path.realpath(__file__)),'test_temp')
+os.makedirs(DIR)
+os.chdir(DIR)
+
+#os.chdir(os.path.dirname(os.path.realpath(__file__)))
 execfile("delay_and_cache.py")

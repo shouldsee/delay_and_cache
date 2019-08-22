3850c8df4d19bb253f09f6589d6b97fdbec191c0
teamchung <teamchung@example.com>
2019-08-22 15:51:15 +0000

    add example

--- test.py
+++ test.py
@@ -3,5 +3,8 @@ DIR= os.path.join(os.path.dirname(os.path.realpath(__file__)),'test_temp')
 if not os.path.isdir(DIR):
     os.makedirs(DIR)
 os.chdir(DIR)
+
+import delay_and_cache
 #os.chdir(os.path.dirname(os.path.realpath(__file__)))
 execfile(DIR + "/../delay_and_cache.py")
+execfile(DIR + "/../example.py")
